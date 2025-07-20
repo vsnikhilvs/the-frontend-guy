c0fa79d43a4cc341eda0260a2ecc17682820cc74
Nikhil V S <vsnikhilvs@gmail.com>
2025-07-20 12:56:05 +0530

    Add resume

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -127,7 +127,7 @@ const CONFIG = {
   },
   resume: {
     fileUrl:
-      'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
+      'public/Nikhil_Vikraman.pdf', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
     'React.js',
